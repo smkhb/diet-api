e02f5a08410f7c5d1b35bc3bc32f546ffa576f8b
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-08-07 10:53:23 -0300

    Database basic setup
    Setting up knex

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
+import { knex } from './databse'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  return 'hello world'
+  const test = await knex('sqlite_schema').select('*')
+  return test
 })
 
 app.listen({ port: 3333 }).then(() => {
